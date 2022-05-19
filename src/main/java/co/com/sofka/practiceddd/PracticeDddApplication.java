f6b49ec42dd7925a8b4eb70d71da1a8d634023c0
Jhonny Castro Clavijo <johnny.castro@misena.edu.co>
2022-05-18 23:04:34 -0500

    Initial commit

---
+++ src/main/java/co/com/sofka/practiceddd/PracticeDddApplication.java
@@ -0,0 +1,13 @@
+package co.com.sofka.practiceddd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeDddApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticeDddApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/com/sofka/practiceddd/PracticeDddApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.sofka.practiceddd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeDddApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
